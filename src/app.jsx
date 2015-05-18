9b2538f7b88360f11ee08cf9ea507fb06e3e9471
Mehul Patel <mehul8@gmail.com>
2015-05-18 16:14:49 -0700

    learned mixins, and componentWillMount()

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,48 @@
-var React = require("react");
+var React = require('react');
+
+var ReactMixin = {
+  getInitialState() {
+    return { count: 0 };
+  },
+  componentWillMount() {
+    console.log('will mount');
+  },
+  incrementCount() {
+    this.setState({ count: this.state.count+1 });
+  }
+};
 
 var App = React.createClass({
-    render() {
-        return <h1>Hello change</h1>;
-    }
-})
+  render() {
+    return (
+      <div>
+        <Button txt="button text" />
+        <br />
+        <Label txt="label text" />
+      </div>
+    );
+  }
+});
 
-React.render(<App/>, document.getElementById('example'));
+var Button = React.createClass({
+  mixins: [ReactMixin],
+  render() {
+    return (
+      <button onClick={this.incrementCount}>{this.props.txt} -- { this.state.count } </button>
+    );
+  }
+});
+
+var Label = React.createClass({
+  mixins: [ReactMixin],
+  componentWillMount() {
+    setInterval(this.incrementCount, 1000);
+  },
+  render() {
+    return (
+      <label>{ this.props.txt } -- { this.state.count }</label>
+    );
+  }
+});
+
+React.render(<App />, document.body);
